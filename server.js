cbcf50afe2487292dc5e8d7ba2209979c90d377b
Laura <lg3295@gmail.com>
2016-04-12 09:47:09 -1000

    returns the new statement

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var redact = require('./redact.js')
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(function(req, res, next){
+  redact(req, res, next);
+});
+
+app.post('/message', function(req, res){
+  res.status(200).send(req.body.message);
+});
+
+var server = app.listen(8080, function(){
+  console.log('App Listening!');
+});
